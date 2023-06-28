54dc8b0b227cc8cb3f7b94aecde35602d3caa884
webcam121 <liuj96121@gmail.com>
2023-06-28 10:51:08 -0400

    feat: fixed serializer

--- microblog/blog/serializers.py
+++ microblog/blog/serializers.py
@@ -13,24 +13,3 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'title', 'content', 'author', 'created_at', 'tags', 'scheduled_time']
         read_only_fields =["author"]
-
-    # def create(self, validated_data):
-    #     tags_data = validated_data.pop('tags')  # change
-    #     post = Post.objects.create(**validated_data)  # change
-    #
-    #     for tag_data in tags_data:
-    #         Tag.objects.create(post=post, **tag_data)  # change
-    #     return post
-    #
-    # def update(self, instance, validated_data):
-    #     tags_data = validated_data.pop('tags')
-    #     tags = (instance.tags).all()
-    #     tags = list(tags)
-    #
-    #     super().update(instance, validated_data)
-    #
-    #     for tag_data in tags_data:
-    #         tag = tags.pop(0)
-    #         tag.name = tag_data.get('name', tag.name)
-    #         tag.save()
-    #     return instance
